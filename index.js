23043ae4d7beb1a06daebd40c193f97438e0a348
unknown <marko.rodriguez@hotmail.es>
2022-06-25 01:09:43 -0500

    build 2

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.listen(process.env.PORT || 5000, () => {
-    console.log('Server is running on port 5000');
+    console.log('Server is running on', process.env.PORT);
 });
 app.get("/", (req, res) => {
     res.send("Hello World");
--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.listen(process.env.PORT || 5000, ()=>{
-    console.log('Server is running on port 5000')
+    console.log('Server is running on', process.env.PORT )
 })
 
 app.get("/", (req:any,res:any)=>{
