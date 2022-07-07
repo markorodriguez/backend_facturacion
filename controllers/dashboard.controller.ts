bd13f6eac6a4cddc4701c203931a5ca34ce6ec41
unknown <marko.rodriguez@hotmail.es>
2022-07-07 14:45:30 -0500

    07-07

---
+++ controllers/dashboard.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+import db from '../config/db';
+
+import moment from "moment"
+
+const express = require('express')
+const dashboardRouter = express.Router()
+
+dashboardRouter.get("/", (req:Request, res:Response)=>{
+
+    const año = moment().year()
+
+    db.query('SELECT * from detalle_factura', (err:any, rows:any) => {
+
+        const filteredTiempo = rows.filter((el:any) => el.tiempoejecucion != null)
+
+        const filtradoPorAno = [...filteredTiempo].filter((factura) => factura.fecha.slice(6, 10) == año)
+
+        const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
+        console.log(filteredTiempo[0].fecha.slice(3, 5))
+
+        const reportes:Array<any> = []
+
+        meses.map((mes, index) => {
+            const filtradoPorMes = [...filtradoPorAno].filter((factura) => factura.fecha.slice(3, 5) == index + 1)
+
+            const gananciasMes = {
+                mes: mes,
+                promedio: filtradoPorMes.length > 0 ? ([...filtradoPorMes].reduce((acc, b) => acc + Number.parseInt(b.tiempoejecucion), 0))/([...filtradoPorMes].length) : 0,
+                ventas: filtradoPorMes.length > 0 ? [...filtradoPorMes].reduce((acc, b) => acc + (Number.parseFloat(b.importetotal) + Number.parseFloat(b.igv)), 0) : 0,
+                efectividad: {
+                    correctas: [...filtradoPorMes].filter((el)=>el.estado == 'FACTURADO').length,
+                    canceladas: [...filtradoPorMes].filter((el)=>el.estado != 'FACTURADO').length
+                }
+
+            }
+            reportes.push(gananciasMes)
+            
+        })
+
+        console.log(reportes)
+        res.json(reportes)
+
+
+    })
+})
+
+export default dashboardRouter;
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import productoRouter from "./controllers/productos.controller"
 import usuariosRouter from "./controllers/usuarios.controller"
 import clienteRouter from "./controllers/clientes.controller"
 import facturasRouter from "./controllers/facturas.controller"
+import dashboardRouter from "./controllers/dashboard.controller"
 
 require('dotenv').config()
 
@@ -25,3 +26,4 @@ app.use("/productos", productoRouter)
 app.use("/usuarios", usuariosRouter)
 app.use("/clientes", clienteRouter)
 app.use("/facturas", facturasRouter)
+app.use("/reportes", dashboardRouter)
